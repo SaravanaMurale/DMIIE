3b80342c1b3c5eab078179369d615361b7b3c5d5
murali <murali.e@sosaley.in>
2021-06-21 12:29:26 +0530

    Admin Drawer Activity

---
+++ app/src/main/java/com/pojo/dmiie/admin/AdminDrawerActivity.java
@@ -0,0 +1,16 @@
+package com.pojo.dmiie.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pojo.dmiie.R;
+
+public class AdminDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_drawer);
+    }
+}
