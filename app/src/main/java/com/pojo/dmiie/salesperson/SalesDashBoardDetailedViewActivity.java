cf2cd65a1bcdf0b0d76f728abb8eb01e187b9749
murali <murali.e@sosaley.in>
2021-07-07 16:31:56 +0530

    Retrofit Commit

--- app/src/main/java/com/pojo/dmiie/salesperson/SalesDashBoardDetailedViewActivity.java
+++ app/src/main/java/com/pojo/dmiie/salesperson/SalesDashBoardDetailedViewActivity.java
@@ -2,23 +2,34 @@ package com.pojo.dmiie.salesperson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.pojo.dmiie.R;
 import com.pojo.dmiie.model.SalesDashBoardDTO;
 
+import java.util.Calendar;
+
 public class SalesDashBoardDetailedViewActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView salesCustName,salesCustMobileNum,salesCustBusOwnNum,salesCustNxtFollowDate,salesCustLastSaleDate;
     Spinner salesCustSpinnerStatus,salesCustSpinnerNeedSample,salesCustSpinnerNeedPriceList,salesCustSpinnerManualVisit,salesCustSpinnerFollowUpStatus;
     EditText salesCustRemark;
+
+    RelativeLayout nxtfollowUpBlock;
+    DatePickerDialog.OnDateSetListener setListenerDateOfBirth;
+    String nxtFollowDateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +49,40 @@ public class SalesDashBoardDetailedViewActivity extends AppCompatActivity implem
         salesCustSpinnerManualVisit=(Spinner)findViewById(R.id.salesCust_manual_visit_detailView);
         salesCustSpinnerFollowUpStatus=(Spinner)findViewById(R.id.salesCust_follow_status_detailView);
 
+        nxtfollowUpBlock=(RelativeLayout)findViewById(R.id.nxtfollowUpBlock);
+
+
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int date = calendar.get(Calendar.DATE);
+
+        nxtfollowUpBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(SalesDashBoardDetailedViewActivity.this, android.R.style.Theme_Holo_Light_Dialog, setListenerDateOfBirth, year, month, date);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListenerDateOfBirth = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                salesCustNxtFollowDate.setText(date);
+
+                nxtFollowDateString=date;
+
+                System.out.println("selectedFromDate " + date);
+
+
+            }
+        };
+
 
         ArrayAdapter<CharSequence> customerStatus = ArrayAdapter.createFromResource(SalesDashBoardDetailedViewActivity.this, R.array.customer_status, android.R.layout.simple_spinner_item);
         customerStatus.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -51,8 +96,8 @@ public class SalesDashBoardDetailedViewActivity extends AppCompatActivity implem
 
         ArrayAdapter<CharSequence> needPriceList = ArrayAdapter.createFromResource(SalesDashBoardDetailedViewActivity.this, R.array.need_price_list, android.R.layout.simple_spinner_item);
         needPriceList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        salesCustSpinnerNeedSample.setAdapter(needPriceList);
-        salesCustSpinnerNeedSample.setOnItemSelectedListener(this);
+        salesCustSpinnerNeedPriceList.setAdapter(needPriceList);
+        salesCustSpinnerNeedPriceList.setOnItemSelectedListener(this);
 
         ArrayAdapter<CharSequence> manualVisitReq = ArrayAdapter.createFromResource(SalesDashBoardDetailedViewActivity.this, R.array.manual_visit, android.R.layout.simple_spinner_item);
         manualVisitReq.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
